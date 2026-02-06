b595af0a9c8ce7cb645c44dd841e76ff6497ccb9
camille kongolo <ksthray@gmail.com>
2026-02-06 20:09:36 +0100

    add suspense to connexion page

--- app/(client)/connexion/page.tsx
+++ app/(client)/connexion/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 import AuthPage from "./components/auth";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const metadata: Metadata = {
   title: "Connexion - Connect Student",
@@ -8,7 +10,17 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="bg-gray-50">
-      <AuthPage />
+      <Suspense fallback={<Loading />}>
+        <AuthPage />
+      </Suspense>
     </main>
+  );
+}
+
+function Loading() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <Skeleton className="w-full h-72 bg-gray-300 rounded-lg" />
+    </div>
   );
 }
